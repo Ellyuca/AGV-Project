fd0f2294b2d6976dd341957e36ed6f8166f16ab2
Cristian <cristiancosci@gmail.com>
2022-08-23 21:12:55 +0200

    work

--- AGV_emotions_attack/attacks_emotions/models/keras_models/mobilenet_model.py
+++ AGV_emotions_attack/attacks_emotions/models/keras_models/mobilenet_model.py
@@ -1,4 +1,5 @@
 import sys, os
+from turtle import shape
 from xml.etree.ElementInclude import include
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from utils import load_externals
@@ -11,7 +12,7 @@ def MobileNet_model(input_shape):
   base_model = tf.keras.applications.MobileNet()
   return base_model
 '''
-
+'''
 def MobileNet_model(input_shape):
   inputs = tf.keras.Input(shape=input_shape)
   x = tf.keras.applications.mobilenet.preprocess_input(inputs*255)
@@ -20,6 +21,18 @@ def MobileNet_model(input_shape):
   x = core(x)
   
   model = tf.keras.Model(inputs=[inputs], outputs=[x])
+
+  return model
+'''
+
+def MobileNet_model(input_shape):
+  inputs = tf.keras.Input(shape=input_shape)
+  x = tf.keras.applications.resnet50.preprocess_input(inputs*255)
+
+  core = tf.keras.applications.ResNet50()
+  x = core(x)
+
+  model = tf.keras.Model(input=[inputs], outputs=[x])
 
   return model
 
