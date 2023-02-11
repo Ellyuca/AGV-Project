0acddac124f0f12727a34852cf6822ed9f88a575
CristianCosci <cristiancosci@gmail.com>
2023-02-11 10:01:17 +0000

    update repo structure

--- AGV_emotions_attack/attacks_emotions/agv/create_test.sh
+++ AGV_emotions_attack/attacks_emotions/agv/create_test.sh
@@ -1,9 +1,9 @@
-# for i in {0..19}
-# do
-#    python attacks_emotions/agv/agv_attack.py -bf TEST/best_jsons -db "IMAGENET-RESNET" -sae_best=True -img_id $i
-# done
-
 for i in {0..19}
+do
+   python attacks_emotions/agv/agv_attack.py -bf TEST/best_jsons -db "IMAGENET-RESNET" -sae_best=True -img_id $i
+done
+
+for i in {0..9}
 do
    python attacks_emotions/agv/plot_fitness_logs.py -img_id $i
 done
