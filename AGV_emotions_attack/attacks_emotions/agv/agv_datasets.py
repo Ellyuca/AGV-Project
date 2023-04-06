7e035077c47521fbec0431d4b2cef7f606c572dd
CristianCosci <cristiancosci@gmail.com>
2023-04-06 10:23:25 +0000

    fixing typos

--- AGV_emotions_attack/attacks_emotions/agv/agv_datasets.py
+++ AGV_emotions_attack/attacks_emotions/agv/agv_datasets.py
@@ -71,7 +71,7 @@ def database_and_model():
     return {
 
         'IMAGENET-RESNET' : [
-            DatasetSubset(Imagenet, nsamples = 10, start_nsamples=8),
+            DatasetSubset(Imagenet, nsamples = 10, start_nsamples=0),
             'resnet',       
         ],            
 
